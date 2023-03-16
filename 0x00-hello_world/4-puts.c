563368ce509c1b0f3e6c034b17325944c0ab4586
yohannes1912 <azyrgy@gmail.com>
2023-03-16 08:30:20 -0700

    Task 4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stio.n>
+/**
+ * main - A program that prints a line with puts function
+ * Return: 0 (Success)
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle, followed by a new line");
+	return (0);
+}
