3238d96e315367225c641892b4a7560a9eda5e85
yohannes1912 <azyrgy@gmail.com>
2023-03-18 03:13:17 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-last_digit.c -o 1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,15 +9,15 @@
 /**
  * main - Prints a text according number
  * Return: Always (Success)
- *
  */
 
 int main(void)
 
 {
 	int n, lastd;
+
 	srand(time(0));
-	n = rand () - RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 	lastd = n % 10;
 
 	if (lastd > 5)
@@ -28,7 +28,7 @@ int main(void)
 	{
 	printf("Last digit of %d is %d and is 0\n", n, lastd);
 	}
-	else if (lastd < 6 &&lastd != 0)
+	else if (lastd < 6 && lastd != 0)
 	{
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
 	}
